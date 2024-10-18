3ae8f98629074ead43c00f93532624b1bbcef8cb
kamimartinez <kamiii.martineeez@gmail.com>
2024-10-18 10:15:06 -0600

    [ADD] CRUD

---
+++ src/routes/student.ts
@@ -0,0 +1,21 @@
+// routes/usuarios.ts
+import { Router } from "express";
+import {
+  createStudent,
+  deleteStudent,
+  getStudents,
+  updateStudent,
+} from "../controllers/student";
+
+const router = Router();
+
+// Regresa todos los alumnos en la base de datos
+router.get("/", getStudents);
+
+router.post("/", createStudent);
+
+router.put("/:id", updateStudent);
+
+router.delete("/:id", deleteStudent);
+
+export default router;
