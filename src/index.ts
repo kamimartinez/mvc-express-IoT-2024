9e52920b1b49dcd3a7f25e7989d57003814c1584
kamimartinez <kamiii.martineeez@gmail.com>
2024-10-31 17:01:21 -0600

    [ADD] Middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { config } from "dotenv";
 import express from "express";
 import studentRouter from "./routes/student";
+import unknownResource from "./middlewares/unknown-resource";
+import testRoutes from "./routes/test";
+import unknownError from "./middlewares/unknown-error";
 
 //Para poder acceder a las variables del ambiente(.env)
 config();
@@ -9,7 +12,13 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/student", studentRouter);
+app.use("/api/v1/student", studentRouter);
+
+app.use("/error", testRoutes);
+
+app.use(unknownResource);
+
+app.use(unknownError);
 
 app.listen(process.env.SERVER_PORT, function () {
   console.log("Escuchando puerto " + process.env.SERVER_PORT);
